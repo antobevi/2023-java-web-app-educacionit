6d5ff665cef7d97e499b85a0e55acf6d3a737e89
antobevi <abevilacqua@frba.utn.edu.ar>
2023-12-25 18:31:22 -0300

    First Commit

---
+++ src/main/java/com/antobevi/javawebappeducacionit/JavaWebAppEducacionitApplication.java
@@ -0,0 +1,13 @@
+package com.antobevi.javawebappeducacionit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebAppEducacionitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebAppEducacionitApplication.class, args);
+	}
+
+}
